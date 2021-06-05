2ce4476904757a3dbd692c7a92f9bade1aec03ee
Ana.Sousa <anasousa314@outlook.com>
2021-06-05 15:04:49 -0300

    adiciona aula26 sobre estrutura de um componente de Classe em React

---
+++ Aula26/aula26/src/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import BaseClasse from './components/BaseClasse';
+import Carro from './components/Carro'
+
+//Pode misturar componente de Classe com componente de Funcional
+function App() {
+
+  const [carro,setCarro]=useState(true)
+  const mostrarOcultarCarro=()=>{
+    setCarro(!carro)
+  }
+  return (
+    <>
+      <h1>Componentes de Classe</h1>
+      {carro ? <Carro fator={10}/> : ''} 
+
+      {/* para chamar componentWillUnmount */}
+      <button onClick={()=>mostrarOcultarCarro()}>
+          Ocultar
+        </button>
+
+      <br/>
+      <br/>
+
+      <BaseClasse/>
+
+    </>
+  );
+}
+
+export default App;
