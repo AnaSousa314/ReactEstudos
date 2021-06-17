d49c415ab5d9abb4c9dd62bd37892ff0035f139a
Ana.Sousa <anasousa314@outlook.com>
2021-06-17 18:55:19 -0300

    adiciona aula32 sobre Componentes funcionais para componentes de classe

---
+++ Aula32/aula32/src/components/Peso.js
@@ -0,0 +1,16 @@
+import React from "react"
+export default class Peso extends React.Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div>
+        <label>
+          Peso
+          <input type="text" value={this.props.p} onChange={(e)=>{this.props.sp(e.target.value)}}/>
+        </label>
+      </div>
+    )
+  }
+}
