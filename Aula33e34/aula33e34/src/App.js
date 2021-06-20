ac4084d564b0988c2572c29f1840c0d11de5b70a
Ana.Sousa <anasousa314@outlook.com>
2021-06-20 19:25:30 -0300

    adiciona aula33e34 sobre criacao de uma calculadora em React parte II

--- Aula33e34/aula33e34/src/App.js
+++ Aula33e34/aula33e34/src/App.js
@@ -48,14 +48,53 @@ function App() {
     setValorTela('');
     setResultado(0);
     setAcumulador(0);
+    return
+  }
+
+  const operacao=(oper)=>{
+    if(oper=='bs'){
+      let vtela=valorTela
+      vtela=vtela.substring(0,(vtela.length-1));
+      setValorTela(vtela);
+      setOperado(false)
+      return
+    }
+
+    try {
+      //Calculo
+      const r=eval(valorTela);//faz o calculo, ex.: digitou na tela 5+3, ele analiza o que foi digitado e retorna o calculo. Mas é uma função perigosa
+      setAcumulador(r);
+      setResultado(r);
+      setOperado(true);
+    } catch (error) {
+      setResultado('ERRO',error);
+    }
   }
 
   //Estilos
-  const cssTela={
+  const App={
     display: 'flex',
+    justifyContent:'center',
+    alignItems:'center'
+  }
+  const cssConteiner={
+    display:'flex',
+    justifyContent:'flex-start',
+    alignItems:'center',
+    flexDirection:'column',
+    width:300,
+    border: '1px solid #000'
+  }
+
+  const cssBotoes={
+    flexDirection:'row',
+    flexWrap:'wrap'
+  }
+  const cssTela={
+    display: 'grid',
     paddingLeft:20,
     paddingRight:20,
-    justifyContent:'center',
+    justifyContent:'flex-start',
     alignItems:'flex-start',
     backgroundColor: '#444',
     flexDirection: 'colum',
@@ -86,11 +125,37 @@ function App() {
   }
 
   return (
-    <>
-      {Tela()}
-      {Btn()}
-  
-    </>
+    <div style={App}>
+      <div style={cssConteiner}>
+        <h3>Calculadora Matemática Simples</h3>
+        {Tela(valorTela,resultado)}
+        <div style={cssBotoes}>
+          {Btn('AC',limparMemoria)}
+          {Btn('(',()=>addDigitoTela('('))}
+          {Btn(')',()=>addDigitoTela(')'))}
+          {Btn('/',()=>addDigitoTela('/'))}
+          {Btn('7',()=>addDigitoTela('7'))}
+          {Btn('8',()=>addDigitoTela('8'))}
+          {Btn('9',()=>addDigitoTela('9'))}
+          {Btn('*',()=>addDigitoTela('*'))}
+          {Btn('4',()=>addDigitoTela('4'))}
+          {Btn('5',()=>addDigitoTela('5'))}
+          {Btn('6',()=>addDigitoTela('6'))}
+          {Btn('-',()=>addDigitoTela('-'))}
+          {Btn('1',()=>addDigitoTela('1'))}
+          {Btn('2',()=>addDigitoTela('2'))}
+          {Btn('3',()=>addDigitoTela('3'))}
+          {Btn('+',()=>addDigitoTela('+'))}
+          {Btn('0',()=>addDigitoTela('0'))}
+          {Btn('.',()=>addDigitoTela('.'))}
+          {Btn('<-',()=>operacao('bs'))}
+          {Btn('=',()=>operacao('='))}
+
+
+          
+        </div>
+      </div>
+    </div>
   );
 }
 
